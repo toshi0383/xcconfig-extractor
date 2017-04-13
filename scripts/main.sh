d28e3528de9f0ec32ad503e3b30bf48c3f423d9c
toshi0383 <t.suzuki326@gmail.com>
2017-04-13 12:40:47 +0900

    Print Usage and Version(--version)

--- scripts/main.sh
+++ scripts/main.sh
@@ -15,6 +15,14 @@
 set -eo pipefail
 
 BASEDIR=$(cd $(dirname $0);pwd)
+if [ $# -eq 0 ];then
+    echo "Usage:"
+    echo "    $(basename $0) [path to pbxproj] [output dir (defaults to '.')]"
+    exit 1
+elif [ "$1" == "--version" ];then
+    echo "xcconfig-extractor 0.1.3"
+    exit
+fi
 PBXPROJ=${1:?}
 XCCONFIG_OUT_DIR=${2:-.}
 $BASEDIR/extract-build-settings-to-xcconfigs.sh $PBXPROJ $XCCONFIG_OUT_DIR
