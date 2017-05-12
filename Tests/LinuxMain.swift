69998f8673721d688a589b477eb8dbc9051f82a9
toshi0383 <t.suzuki326@gmail.com>
2017-05-12 21:45:06 +0900

    Refactor under Tests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,5 +1,15 @@
+// Generated using Sourcery 0.6.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
 import XCTest
+extension FunctionsTests {
+  static var allTests: [(String, (FunctionsTests) -> () throws -> Void)] = [
+    ("testCommonElements", testCommonElements),
+  ]
+}
 
 XCTMain([
-    testCase(FunctionsTests.allTests),
+  testCase(FunctionsTests.allTests),
 ])
+
