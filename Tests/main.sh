9c9c86ec3641f5858aaeb3ffb1e1442dea058f0e
toshi0383 <t.suzuki326@gmail.com>
2017-06-02 20:32:46 +0900

    Update Test/main.sh
    Test "Build Settings Validation" automatically for all fixtures

--- Tests/main.sh
+++ Tests/main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eo pipefail
+
 basedir=$(cd $(dirname $0); pwd)
 cd $basedir/..
 make build
@@ -18,10 +19,20 @@ do
     proj=${dir}/${proj}
     cd ${proj}/..
     tmpdir="${PWD}/tmp/xcconfig-extractor-configs"
+    # testScheme=$(xcodebuild -list | grep -A1 Schemes | tail -1 | tr -d ' ') # TODO: Test against all scheme, creating shared ones on demand..
+    xcodebuild -showBuildSettings > before
     COMMAND="${XCCONFIG_EXTRACTOR} ${proj} ${tmpdir}"
     $COMMAND && ls -l ${tmpdir}
     if [ $? -ne 0 ];then
         echo Execution Failed: $COMMAND
+        FAILURES="$FAILURES $proj"
+        STATUS=1
+        continue
+    fi
+    xcodebuild -showBuildSettings > after
+    diff before after
+    if [ $? -ne 0 ];then
+        echo "xcodebuild -showBuildSettings between before and after execution differ."
         FAILURES="$FAILURES $proj"
         STATUS=1
         continue
