184a06f39a29531485ef2a2bd27cd10a3ee5c0cb
tokijh <tokijh@naver.com>
2019-10-12 23:35:27 +0900

    Update Package.swift for using swift 5.1

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:4.2
-// Managed by ice
+// swift-tools-version:5.1
 
 import PackageDescription
 
@@ -9,9 +8,9 @@ let package = Package(
         .executable(name: "xcconfig-extractor", targets: ["xcconfig-extractor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-        .package(url: "https://github.com/kylef/Commander", from: "0.9.0"),
-        .package(url: "https://github.com/tuist/XcodeProj", from: "7.0.1"),
+        .package(url: "https://github.com/kylef/PathKit", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/kylef/Commander", .upToNextMajor(from: "0.9.0")),
+        .package(url: "https://github.com/tuist/XcodeProj", .upToNextMajor(from: "7.1.0")),
     ],
     targets: [
         .target(name: "xcconfig-extractor", dependencies: ["Utilities", "PathKit", "Commander", "XcodeProj"]),
