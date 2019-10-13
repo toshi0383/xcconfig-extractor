bbfbb8d80f28fad6780baf780e7a3d23c150cf8f
toshi0383 <t.suzuki326@gmail.com>
2019-10-13 21:26:35 +0900

    improve test script

--- Tests/main.sh
+++ Tests/main.sh
@@ -2,8 +2,9 @@
 set -eo pipefail
 
 basedir=$(cd $(dirname $0); pwd)
-cd $basedir/..
-make build
+SRCROOT="$basedir/.."
+cd $SRCROOT
+swift build
 XCCONFIG_EXTRACTOR=${PWD}/.build/x86_64-apple-macosx/debug/xcconfig-extractor
 if [ ! -x $XCCONFIG_EXTRACTOR ];then
     echo Executable not found!: $XCCONFIG_EXTRACTOR
@@ -11,16 +12,15 @@ if [ ! -x $XCCONFIG_EXTRACTOR ];then
 fi
 
 set +e
-dir=${PWD}
 for proj in $(find Fixtures -name "*xcodeproj" | grep -v archive)
 do
     echo "=============================="
     echo $proj
-    proj=${dir}/${proj}
+    proj=${SRCROOT}/${proj}
     cd ${proj}/..
     tmpdir="${PWD}/tmp/xcconfig-extractor-configs"
     # testScheme=$(xcodebuild -list | grep -A1 Schemes | tail -1 | tr -d ' ') # TODO: Test against all scheme, creating shared ones on demand..
-    xcodebuild -showBuildSettings > before
+    xcodebuild -showBuildSettings 2> /dev/null > before
     COMMAND="${XCCONFIG_EXTRACTOR} ${proj} ${tmpdir}"
     echo $COMMAND
     $COMMAND && ls -l ${tmpdir}
@@ -30,7 +30,7 @@ do
         STATUS=1
         continue
     fi
-    xcodebuild -showBuildSettings > after
+    xcodebuild -showBuildSettings 2> /dev/null > after
     diff before after
     if [ $? -ne 0 ];then
         echo "xcodebuild -showBuildSettings between before and after execution differ."
@@ -43,5 +43,9 @@ done
 if [ $STATUS ];then
     echo TEST FAILED
     echo Failing Tests: $FAILURES
+else
+    cd $SRCROOT/Fixtures
+    git reset --hard @
+    git clean -df
 fi
 exit $STATUS
