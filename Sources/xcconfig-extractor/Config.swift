7feffcb344e79299b87e76f7d3d9c4bc8f741210
toshi0383 <t.suzuki326@gmail.com>
2019-10-13 20:58:32 +0900

    update version

--- Sources/xcconfig-extractor/Config.swift
+++ Sources/xcconfig-extractor/Config.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 struct Config {
-    static let version = "0.5.0"
+    static let version = "0.6.0"
     let isIncludeExisting: Bool
     init(isIncludeExisting: Bool) {
         self.isIncludeExisting = isIncludeExisting
